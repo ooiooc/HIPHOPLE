8e6862ed8448b8a63bbd8daf4d5b69831c8c6aa9
ooiooc <70251769+ooiooc@users.noreply.github.com>
2020-12-12 14:26:16 +0900

    커뮤니티 글 작성시 writerid DB에 추가

--- HIPHOPLE/src/main/java/com/project/hiphople/BoardController.java
+++ HIPHOPLE/src/main/java/com/project/hiphople/BoardController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +18,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.project.domain.Criteria;
+import com.project.domain.MemberVO;
 import com.project.domain.PageDTO;
 import com.project.domain.BoardAttachVO;
 //import com.myspring.domain.BoardAttachVO;
 //import com.myspring.domain.PageDTO;
 import com.project.domain.BoardVO;
 import com.project.service.BoardService;
+import com.project.service.MemberService;
 
 @Controller
 /* @RequestMapping("board") */
@@ -31,6 +35,9 @@ public class BoardController{
 	@Autowired
  	private BoardService service;
 
+	@Autowired
+	private MemberService memservice;
+	
  	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
  	
  	//공지사항 글쓰기 화면
@@ -47,6 +54,7 @@ public class BoardController{
  			logger.info("글쓰기 post");
  			logger.info("BoardVO에 저장되어 있는 값 :" + board);
  			
+ 			board.setWriter("힙합엘이");
  			service.insert(board); //insert SQL
  			model.addAttribute("result", "success");
  			
@@ -117,21 +125,32 @@ public class BoardController{
 	
 	}
 
-	//community 게시판 글쓰기 화면
+	//Community 게시판 글쓰기 화면
 	@GetMapping("community/write")
-	public String boardWrite() {
+	public String boardWrite(MemberVO mem, Model model) {
 		logger.info("summernote 확인");
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String userid = authentication.getName(); //로그인된 username
+		
+		logger.info("current username : {}", userid);
+		
+		mem.setUserid(userid);
+		model.addAttribute("member", memservice.memberInfo(mem));
+		
 		return "community/write";
 	}
 	
-	//community 글쓰기 처리
+	//Community 글쓰기 처리
 	 @RequestMapping(value="community/insertComm", method = RequestMethod.POST)
-	 public String insertComm(BoardVO board, Model model) throws Exception{
-	 			
+	 public String insertComm(BoardVO board, Model model) throws Exception{	 			
 	 		logger.info("커뮤니티 글쓰기 post");
-
-			/* board.setCategory("community"); */
 	 		logger.info("BoardVO에 저장되어 있는 값 :" + board);
+	 		
+	 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			String userid = authentication.getName();
+	 		board.setWriterid(userid); //current user 저장
+	 		
 	 		service.insertComm(board); //insert SQL
 	 		model.addAttribute("result", "success");
 	 			
@@ -139,13 +158,42 @@ public class BoardController{
 
 	 }
 	 
-	//community 상세페이지
+	//Community 상세페이지
 	@RequestMapping(value = "community/view", method = RequestMethod.GET)
 	public void viewComm(BoardVO board, @ModelAttribute("cri") Criteria cri, Model model) throws Exception{
 		logger.info("상세글 페이지 get" + board);
 		logger.info("상세글 페이지 get" + cri);
 			model.addAttribute("view", service.selectComm(board));
 	}
+	
+ 	//Community 수정 화면
+ 	@RequestMapping(value="community/update", method = RequestMethod.GET)
+ 		public void modifyviewcomm(BoardVO board, @ModelAttribute("cri") Criteria cri, Model model) throws Exception{
+ 			logger.info("커뮤니티 수정 페이지");
+ 			model.addAttribute("update", service.selectComm(board));	
+ 	}
+ 	
+ 	//Community 수정 기능
+ 	@RequestMapping(value="community/update.do", method = RequestMethod.POST)
+	public String modifyComm(BoardVO board, Criteria cri, RedirectAttributes rttr) throws Exception {
+		logger.info("커뮤니티 글 수정" + board);
+		service.updateCom(board);
+		
+		//수정했을때 아래의 내용 전송
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addFlashAttribute("msg", "SUCCESS!");
+		return "redirect:/community/list"; 
+	}
+ 	
+	//공지사항 삭제
+ 	@RequestMapping(value = "community/delete", method = RequestMethod.POST)
+	public String removeComm(BoardVO board, Criteria cri, RedirectAttributes rttr) throws Exception{
+		logger.info("삭제 post");
+		service.deleteCom(board);
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addFlashAttribute("msg", "DSUCCESS!");
+		return "redirect:/notice/list";
+	}	
 	 	
 	//community 리스트 화면 페이징x
  	@RequestMapping(value = "community/list", method = RequestMethod.GET)
