431e3cdda9d41f3c26568367b15ef6de8f9a1b47
ooiooc <70251769+ooiooc@users.noreply.github.com>
2021-01-11 17:35:57 +0900

    게시글 삭제

--- HIPHOPLE/src/main/java/com/project/hiphople/ArticleController.java
+++ HIPHOPLE/src/main/java/com/project/hiphople/ArticleController.java
@@ -14,13 +14,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.project.domain.ArticleVO;
 import com.project.domain.BoardAttachVO;
-import com.project.domain.BoardVO;
 import com.project.domain.Criteria;
 import com.project.domain.MemberVO;
 import com.project.domain.PageDTO;
@@ -39,7 +39,7 @@ public class ArticleController {
 
  	private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
 
-	//Interview 리스트 화면 + 페이징
+	//contents 리스트 화면 + 페이징
 	/*
 	@RequestMapping(value = "list", method = RequestMethod.GET) public void
 	listGet(Model model, Criteria cri) throws Exception{ //모델 안에 넣어주기 위해서(select된 내용을 화면에 보여주기) 위에 model 선언 
@@ -48,18 +48,22 @@ public class ArticleController {
 		model.addAttribute("pageMaker", new PageDTO(cri,service.getTotalCount(cri))); }
 	*/ 
  	
-	//interview 게시판 리스트 화면 (페이징 o)
+	// contents 게시판 리스트 화면 (페이징 o)
 	@RequestMapping(value="list", method = RequestMethod.GET)
 	public void articleList(Model model, Criteria cri) throws Exception{ 
 			logger.info("article list get");
-			/* model.addAttribute("list", service.listAll()); */
+			
+			//List<ArticleVO> article= service.listAll();
+			//
+			//model.addAttribute("contents", article);
 			model.addAttribute("list", service.postlistPage(cri));	
 			model.addAttribute("pageMaker", new PageDTO(cri, service.getTotalCount(cri)));
 			model.addAttribute("category", service.categoryList());
+			//model.addAttribute("upload", service.getAttachlist(bno));
 			
 	}
 	
-	//Interview 게시물 상세페이지 화면 
+	// contents 게시물 상세페이지 화면 
 	@RequestMapping(value="view", method = RequestMethod.GET)
 	public void articleView(ArticleVO vo,  @ModelAttribute("cri") Criteria cri, Model model, MemberVO mem) throws Exception{ 
 		logger.info("상세글 페이지 get" + vo);
@@ -73,11 +77,12 @@ public class ArticleController {
 		MemberVO userinfo = new MemberVO();
 		
 		if(userid != "anonymousUser") {
-			logger.info("현재 접속중인 회원 아이디는" + userid + ("입니다"));
+			logger.info("현재 접속중인 회원 아이디는" + userid + "입니다");
 			userinfo.setUserid(userid);
 			userinfo = meservice.memberInfo(userinfo);
 			logger.info("현재 접속중인 회원정보 조회 : " + userinfo);
-			model.addAttribute("user", userinfo.getUsername());
+			model.addAttribute("writer", userinfo.getUsername());
+			model.addAttribute("userid", userinfo.getUserid());
 			}else {
 				userinfo.setUsername("비회원");
 				logger.info(userinfo.getUsername());
@@ -86,13 +91,13 @@ public class ArticleController {
 		model.addAttribute("view", service.select(vo));
 	}
 	
-	//Interview 게시판 글쓰기 화면 
+	// contents 게시판 글쓰기 화면 
 	@RequestMapping(value="write", method = RequestMethod.GET)
 	public void writeView() throws Exception{ 
 			logger.info("article 글쓰기");
 	}
 
-	//interview 글쓰기 기능
+	// contents 글쓰기 기능
 	@RequestMapping(value="insert", method = RequestMethod.POST)
 	public String insertPost(ArticleVO vo, Model model) throws Exception{
 	 		logger.info("article 게시판 글쓰기 post");
@@ -100,12 +105,12 @@ public class ArticleController {
 	 		
  			vo.setWriter("힙합엘이");
 	 		service.insert(vo); //insert SQL
-	 		model.addAttribute("result", "success");
+	 		//model.addAttribute("result", "success");
 	 			
 	 		return "redirect:/contents/list";
 	 	}
 	
-	//contents 수정
+	// contents 수정
 	@RequestMapping(value="update", method = RequestMethod.GET)
 	 	public void modifyGet(ArticleVO vo, @ModelAttribute("cri") Criteria cri, Model model) throws Exception{
 	 			logger.info("contents 업데이트 화면 get");
@@ -113,7 +118,7 @@ public class ArticleController {
 	 			logger.info("update get ::::: " + vo);
 	 	}
 	 
-	//contents 수정 기능
+	// contents 수정 기능
 	@RequestMapping(value="update.do", method = RequestMethod.POST)
 		public String modifyPost(ArticleVO vo, Criteria cri, RedirectAttributes rttr) throws Exception {
 			logger.info("update post" + vo);
@@ -125,7 +130,7 @@ public class ArticleController {
 			return "redirect:/contents/list"; 
 		}
 	 
-	//첨부파일 리스트 가져오기
+	// 첨부파일 리스트 가져오기
 	@RequestMapping(value = "getAttachlist", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<List<BoardAttachVO>> getAttachlist(int bno){
 		logger.info("getAttachlist = " + bno);
@@ -133,5 +138,14 @@ public class ArticleController {
 		return new ResponseEntity<>(service.getAttachlist(bno), HttpStatus.OK);
 	}
 	
+	// 글 삭제
+	@RequestMapping(value = "delete", method = RequestMethod.POST)
+	public String removeComm(ArticleVO board, Criteria cri, RedirectAttributes rttr) throws Exception{
+		logger.info("contents delete post");
+		service.delete(board);
+		//rttr.addAttribute("pageNum", cri.getPageNum());
+		//rttr.addFlashAttribute("msg", "DSUCCESS!");
+		return "redirect:/contents/list";
+	}	
 
 }
