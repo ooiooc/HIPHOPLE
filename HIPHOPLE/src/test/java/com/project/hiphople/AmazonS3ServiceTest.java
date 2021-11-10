8896cfe64412bb61add8ce226c09d5131b1fffc7
ooiooc <shinjh1102@naver.com>
2021-11-10 18:03:40 +0900

    test: deleting a file

--- HIPHOPLE/src/test/java/com/project/hiphople/AmazonS3ServiceTest.java
+++ HIPHOPLE/src/test/java/com/project/hiphople/AmazonS3ServiceTest.java
@@ -1,30 +1,48 @@
 package com.project.hiphople;
 
+import java.io.IOException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.project.service.S3Service;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-
 public class AmazonS3ServiceTest {
-	private static final Logger logger = LoggerFactory.getLogger(S3UploadController.class);
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private S3Service s3service;
 	
+	@Value("${s3.Url}")
+	private String url;
+	
+	@Value("${s3.Bucket}")
+	private String bucket;
+	
 	// 파일 삭제
 	@Test 
-	public void deleteTest() {
-		String filePath = "https://hiphoplebucket.s3.ap-northeast-2.amazonaws.com/upload/9a1f21ea-5186-436a-b1bc-249cbfa26d28_clairocp.jpg";
-		s3service.deleteFile(filePath);
-		logger.info("삭제 완료 : " + filePath);
+	public void 파일삭제_테스트() throws IOException {
+		String filePath = "";
+		s3service.deleteFile(bucket, filePath);
+		
+		logger.info("bucket : " + bucket);
+		logger.info("fileName : " + filePath);
 	}
 	
 	
